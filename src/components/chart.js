2947502a6f52ca30cf7ccbcd78b9df0bd2157d67
hariswb <hariswb@gmail.com>
2020-09-24 22:48:35 +0700

    Add working mouse event of graph categories

--- src/components/chart.js
+++ src/components/chart.js
@@ -2,83 +2,128 @@ import React, {useEffect,useState,useRef} from "react";
 import ReactDOM from "react-dom";
 import * as d3 from "d3"
 
+const drag = simulation => {
+	function dragstarted(event) {
+		if (!event.active) simulation.alphaTarget(0.3).restart();
+		event.subject.fx = event.subject.x;
+		event.subject.fy = event.subject.y;
+	}
+	function dragged(event) {
+		event.subject.fx = event.x;
+		event.subject.fy = event.y;
+	}
+	function dragended(event) {
+		if (!event.active) simulation.alphaTarget(0);
+		event.subject.fx = null;
+		event.subject.fy = null;
+	}
+	return d3.drag()
+		.on("start", dragstarted)
+		.on("drag", dragged)
+		.on("end", dragended);
+}
+
+const nodeColor=(category)=> {
+  	const scale = d3.scaleOrdinal(d3.schemeCategory10)
+  	// console.log(category)
+  	return d => scale(d[category]);
+}
 
 function Chart(props){
-	const svgStyle = {border: "2px solid gold",
-	backgroundColor: "grey"}
-
 	const width = 500
 	const height = 500
-
 	const ref = useRef()
-	const drag = simulation => {
-		  function dragstarted(event) {
-		    if (!event.active) simulation.alphaTarget(0.3).restart();
-		    event.subject.fx = event.subject.x;
-		    event.subject.fy = event.subject.y;
-		  }
-		  function dragged(event) {
-		    event.subject.fx = event.x;
-		    event.subject.fy = event.y;
-		  }
-		  function dragended(event) {
-		    if (!event.active) simulation.alphaTarget(0);
-		    event.subject.fx = null;
-		    event.subject.fy = null;
-		  }
-		  return d3.drag()
-		      .on("start", dragstarted)
-		      .on("drag", dragged)
-		      .on("end", dragended);
-		}
+	const [focus,setFocus] = useState("")	
+	const [init,setInit] = useState(0);
 	useEffect(() => {
-	    const svg = d3.select(ref.current)
-	    			      .attr("viewBox", [0, 0, width, height]);
-		
-		const links = props.edges.map(d => Object.create({"source": d[0],"target":d[1],"value":1}))
-  		const nodes = props.nodes.map(d => Object.create(d));
+		console.log(props.category,init)
 
-	    const simulation = d3.forceSimulation(nodes)
-              .force("center", d3.forceCenter(width/2, height/2))                  
-              .force("charge", d3.forceManyBody())
-              .force("link", d3.forceLink(links).id(d => d.character))
+		if(init < 1 || props.category == "spanning_tree"){
+			const edges = props.category == "spanning_tree"?props.spanning_tree:props.edges
+			
+			// console.log(edges)
+			const svg = d3.select(ref.current)
+		    			  .attr("viewBox", [0, 0, width, height])
+		    const layer1 = svg.append('g');
+			const layer2 = svg.append('g');
+			const layer3 = svg.append('g');
+			const rect = layer1
+							.append("rect")
+		    				.attr("x1", 0)
+			        		.attr("y1", 0)
+			        		.attr("width", width)
+	      					.attr("height", height)
+		    				.attr("fill","white")
+		    				.on("click",function(){
+		    					d3.select(this).text(d=> setFocus(""))})
 
-        const link = svg.append("g")
-	      .attr("stroke", "#999")
-	      .attr("stroke-opacity", 0.6)
-	      .selectAll("line")
-	      .data(links)
-	      .join("line")
-	      .attr("stroke-width", d => Math.sqrt(d.value));
+			const links = edges.map(d => Object.create({"source": d[0],"target":d[1],"value":1}))
+	  		const nodes = props.nodes.map(d => Object.create(d));
 
-	    const node = svg.append("g")
-	      .attr("stroke", "#fff")
-	      .attr("stroke-width", 1.5)
-	      .selectAll("circle")
-	      .data(nodes)
-	      .join("circle")
-	      .attr("r", 4)
-	      .attr("fill", "black")
-	      .call(drag(simulation));
+		    const simulation = d3.forceSimulation(nodes)
+	              .force("center", d3.forceCenter(width/2, height/2))                  
+	              .force("charge", d3.forceManyBody())
+	              .force("link", d3.forceLink(links).id(d => d.character))
 
-      	simulation.on("tick", () => {
-		    link
-		        .attr("x1", d => d.source.x)
-		        .attr("y1", d => d.source.y)
-		        .attr("x2", d => d.target.x)
-		        .attr("y2", d => d.target.y);
+	        const link = layer2
+		      .attr("stroke", "#999")
+		      .attr("stroke-opacity", 0.6)
+		      .selectAll("line")
+		      .data(links)
+		      .join("line")
+		      .attr("stroke-width", d => Math.sqrt(d.value));
 
-		    node
-		        .attr("cx", d => d.x)
-		        .attr("cy", d => d.y);
-		  });
-      	
-  	}, [])
+		    const node = layer3
+		      .attr("stroke", "#fff")
+		      .attr("stroke-width", 1.5)
+		      .selectAll("circle")
+		      .data(nodes)
+		      .join("circle")
+		      .attr("r", 7)
+		      .attr("fill", nodeColor(props.category))
+		      .call(drag(simulation))
+		      .on("click",function(){ 
+		      	d3.select(this).text(d=> setFocus(d.character))})
+
+	      	simulation.on("tick", () => {
+			    link.attr("x1", d => d.source.x)
+			        .attr("y1", d => d.source.y)
+			        .attr("x2", d => d.target.x)
+			        .attr("y2", d => d.target.y);
+			    node.attr("cx", d => d.x)
+			        .attr("cy", d => d.y);
+			  });
+	      	if(props.category=="spanning_tree"){
+	      		setInit(0)
+	      	}else{
+	      		setInit(1)
+	      	}
+      	}else if(init > 0){
+      		d3.selectAll("circle")
+      		  .attr("fill", nodeColor(props.category))  
+      	}else if(init > 0 && props.category == "spanning_tree"){
+      		setInit(0)
+      		console.log("oooooooiiii")
+      	}
+  	}, [props.category])
+
+
+	function getNodeIndex(charName){
+		return props.nodes.findIndex((node)=>node.character == charName)
+	}
+
+	function getNodeInfo(charIndex){
+		return charIndex >= 0? props.nodes[charIndex].info:"none"
+	}
+
 	return(
 			<div className="chartbox">
+				<div>
+					<h3>{focus}</h3>
+					<h4>{getNodeInfo(getNodeIndex(focus))}</h4>
+				</div>
 				<svg 
 		      		ref={ref}
-		      		style={svgStyle}
 		      	/>
 	      	</div>
 	      	)
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,19 +3,40 @@ import ReactDOM from "react-dom";
 import Chart from "./chart.js"
 import lesmischars from "../data/lesmischars.json"
 import lesmisgraph from "../data/lesmisgraph.json"
+
+const lesmisChars = JSON.parse(lesmischars)
+const lesmisGraph = lesmisgraph
+
+const menu = [	
+				"plot_summary", 
+				"degree_centrality", 
+				"eigenvector_centrality", 
+				"betweenness_centrality", 
+				"closeness_centrality",
+				"community", 
+				"shortest_path",
+				"spanning_tree",
+				]
+// console.log(lesmisGraph)
 function Dashboard(props) {
-	const lesmisChars = JSON.parse(lesmischars)
-	const lesmisGraph = lesmisgraph
-	// console.log(lesmisChars)
+	const [graphCategory,setGraphCategory] =useState("plot_summary")
+
+	const menuButtons = menu.map(d=><li key={d}><button onClick={()=>setGraphCategory(d)}>{d}</button></li>)
 	return (
 	  	<div className="container">
 	  		<h1>Hello, {props.name}</h1>
+	  		<div><ul>{menuButtons}</ul></div>
 	  		<Chart nodes={lesmisChars}
 	  			   edges={lesmisGraph["edges"]}
+	  			   spanning_tree={lesmisGraph["spanning_tree"]}
+	  			   category={graphCategory}
+	  			   menu={menu}
 	  			   />
 	  	</div>
 	);
 
 }
+
+
 
 export default Dashboard;
